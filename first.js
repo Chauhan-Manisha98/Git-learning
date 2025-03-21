579f8845cdcd804eb8242a65decb5714eacaaf48
Manisha Chauhan <manishachauhan9915@gmail.com>
2025-03-21 08:18:29 +0530

    Payment updated after footer

--- first.js
+++ first.js
@@ -4,5 +4,10 @@ console.log(button);
 const login = "Login page added";
 console.log(login);
 
+<<<<<<< HEAD
 const footer = "Fotter added in our website";
 console.log(footer);
+=======
+const Payment = "Integrated the payment gateway";
+console.log(Payment);
+>>>>>>> 989d2c7 (Payment Added)
