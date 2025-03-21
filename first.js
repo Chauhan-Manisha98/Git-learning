78ea30dd0f31161de04e963b89924b96dfb3f848
Manisha Chauhan <manishachauhan9915@gmail.com>
2025-03-21 08:21:02 +0530

    upi done

--- first.js
+++ first.js
@@ -4,10 +4,11 @@ console.log(button);
 const login = "Login page added";
 console.log(login);
 
-<<<<<<< HEAD
 const footer = "Fotter added in our website";
 console.log(footer);
-=======
 const Payment = "Integrated the payment gateway";
 console.log(Payment);
->>>>>>> 989d2c7 (Payment Added)
+
+
+const upi = "Integrated the UPI";
+console.log(upi);
